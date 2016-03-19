d62a7ef47518c7dc8eb8477497eb04775abd279a
maklumi <maklumi@hotmail.com>
2016-03-19 14:38:39 +0800

    thered commit

--- app/src/main/java/com/example/homepc/addressbookdeitel/AddressBook.java
+++ app/src/main/java/com/example/homepc/addressbookdeitel/AddressBook.java
@@ -2,13 +2,16 @@ package com.example.homepc.addressbookdeitel;
 
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,11 +21,35 @@ import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.UUID;
+
+import java.util.logging.LogRecord;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.widget.Toast;
+
 public class AddressBook extends AppCompatActivity {
 
     public static final String ROW_ID = "row_id";
     private ListView contactListView;
     private CursorAdapter contactAdapter;
+
+    public static final UUID MY_UUID =
+            UUID.fromString("a1052349-fcdf-4d89-8d89-0bfe9d0d7cc7");
+    private static String TAG = AddressBook.class.getName();
+    private static final String NAME = "AddressBookBluetooth";
+    private static final int ENABLE_BLUETOOTH = 1;
+    private static final int REQUEST_DISCOVERABILITY = 2;
+    private BluetoothAdapter bluetoothAdapter = null;
+    private boolean userAllowedBluetooth = true;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +69,19 @@ public class AddressBook extends AppCompatActivity {
         contactListView.setAdapter(contactAdapter);
         //   setListAdapter(contactAdapter);
 
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        handler = new Handler();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetoothIntent, ENABLE_BLUETOOTH);
+        }
 
         new GetContactsTask().execute((Object[]) null);
 
@@ -92,8 +127,24 @@ public class AddressBook extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Intent addNewContact = new Intent(AddressBook.this, AddEditContact.class);
-        startActivity(addNewContact);
+        switch (item.getItemId()) {
+            case R.id.addContactItem:
+                Intent addNewContact = new Intent(AddressBook.this, AddEditContact.class);
+                startActivity(addNewContact);
+                break;
+            case R.id.receiveContactItem:
+                if (bluetoothAdapter.isEnabled()) {
+                    Intent requestDiscoverabilityIntent = new Intent(
+                            BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE
+                    );
+                    startActivityForResult(requestDiscoverabilityIntent,
+                            REQUEST_DISCOVERABILITY);
+                } else {
+                    Toast.makeText(this, R.string.no_bluetooth, Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -105,6 +156,114 @@ public class AddressBook extends AppCompatActivity {
             startActivity(viewContact);
         }
     };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case ENABLE_BLUETOOTH:
+                if (resultCode == RESULT_OK) {
+                    Toast.makeText(this, R.string.bluetooth_enabled, Toast.LENGTH_LONG).show();
+                } else {
+                    userAllowedBluetooth = false;
+                    Toast.makeText(this, R.string.no_bluetooth, Toast.LENGTH_LONG).show();
+                }
+                break;
+
+            case REQUEST_DISCOVERABILITY:
+                if (resultCode != RESULT_CANCELED) {
+                    listenForContact();
+                } else {
+                    Toast.makeText(this, R.string.no_discoverability, Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+
+    private void listenForContact() {
+        ReceiveContactTask task = new ReceiveContactTask();
+        task.execute((Object[]) null);
+    }
+
+    // thread that listen for incoming connection request
+    private class ReceiveContactTask extends AsyncTask<Object,Object,Object> {
+
+        private BluetoothServerSocket serverSocket;
+        private BluetoothSocket socket;
+        @Override
+        protected Object doInBackground(Object... params) {
+            try {
+                serverSocket = bluetoothAdapter.listenUsingRfcommWithServiceRecord(
+                        NAME,MY_UUID
+                );
+
+                displayToastViaHandler(AddressBook.this, handler, R.string.waiting_contact);
+
+                BluetoothSocket socket= serverSocket.accept();
+
+                InputStream inputStream = socket.getInputStream();
+
+                byte[] buffer = new byte[1024]; //bytes array to hold data
+                int bytesRead; //number of bytes read
+
+                bytesRead = inputStream.read(buffer);
+
+                if (bytesRead != -1) {
+                    DatabaseConnector databaseConnector = null;
+
+                    // convert to JSON object
+                    try {
+                        JSONObject contact  = new JSONObject(new String(buffer, 0, buffer.length));
+
+                        databaseConnector = new DatabaseConnector(getBaseContext());
+
+                        databaseConnector.open();
+
+                        databaseConnector.insertContact(
+                                contact.getString("name"),
+                                contact.getString("email"),
+                                contact.getString("phone"),
+                                contact.getString("street"),
+                                contact.getString("city")
+                        );
+
+                        //update contact list
+                        new GetContactsTask().execute((Objects[]) null);
+                        displayToastViaHandler(AddressBook.this, handler, R.string.contact_received);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        displayToastViaHandler(AddressBook.this, handler, R.string.contact_not_received);
+
+                    } finally {
+                        if (databaseConnector != null) databaseConnector.close();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (serverSocket != null) serverSocket.close();
+                    if (socket != null) socket.close();
+                } catch (IOException e) {
+                    Log.e(TAG, e.toString());
+                }
+            }
+            return null;
+        }
+    }
+
+    public static void displayToastViaHandler(final Context context, Handler handler, final int stringID) {
+        handler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(context, stringID, Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
 
 
 }
--- app/src/main/java/com/example/homepc/addressbookdeitel/ViewContact.java
+++ app/src/main/java/com/example/homepc/addressbookdeitel/ViewContact.java
@@ -2,11 +2,16 @@ package com.example.homepc.addressbookdeitel;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -14,10 +19,25 @@ import android.support.annotation.Nullable;
 
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
 /**
  * Created by HomePC on 18/3/2016.
  */
-public class ViewContact extends Activity {
+public class ViewContact extends AppCompatActivity {
+    private static final String TAG = ViewContact.class.getName();
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private BluetoothAdapter bluetoothAdapter = null;
+    private Handler handler;
+
     private long rowID;
     private TextView nameTextView;
     private TextView phoneTextView;
@@ -25,10 +45,13 @@ public class ViewContact extends Activity {
     private TextView streetTextView;
     private TextView cityTextView;
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_contact);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.view_contact_toolbar);
+        setSupportActionBar(toolbar);
 
         nameTextView = (TextView) findViewById(R.id.nameTextView);
         phoneTextView = (TextView) findViewById(R.id.phoneTextView);
@@ -37,7 +60,18 @@ public class ViewContact extends Activity {
         cityTextView = (TextView) findViewById(R.id.cityTextView);
 
         Bundle extras = getIntent().getExtras();
-        rowID = extras.getLong("row_id");
+        rowID = extras.getLong(AddressBook.ROW_ID);
+
+        //get the bluetooth adapter
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        handler = new Handler();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.view_contact_menu, menu);
+        return true;
     }
 
     @Override
@@ -79,5 +113,149 @@ public class ViewContact extends Activity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.editItem:
+                Intent addEditContact = new Intent(this, AddEditContact.class);
 
+                addEditContact.putExtra(AddressBook.ROW_ID, rowID);
+                addEditContact.putExtra("name", nameTextView.getText());
+                addEditContact.putExtra("phone", phoneTextView.getText());
+                addEditContact.putExtra("email", emailTextView.getText());
+                addEditContact.putExtra("street", streetTextView.getText());
+                addEditContact.putExtra("city", cityTextView.getText());
+                startActivity(addEditContact);
+                break;
+
+            case R.id.deleteItem:
+                deleteContact();
+                break;
+
+            case R.id.transferItem:
+                if (bluetoothAdapter.isEnabled()) {
+                    Intent serverIntent = new Intent(this, DeviceChooser.class);
+                    startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+                } else {
+                    Toast.makeText(this, R.string.no_bluetooth, Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // delete a contact
+    private void deleteContact()
+    {
+        // create a new AlertDialog Builder
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(ViewContact.this);
+
+        builder.setTitle(R.string.confirmTitle); // title bar string
+        builder.setMessage(R.string.confirmMessage); // message to display
+
+        // provide an OK button that simply dismisses the dialog
+        builder.setPositiveButton(R.string.button_delete,
+                new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int button)
+                    {
+                        final DatabaseConnector databaseConnector =
+                                new DatabaseConnector(ViewContact.this);
+
+                        // create an AsyncTask that deletes the contact in another
+                        // thread, then calls finish after the deletion
+                        AsyncTask<Long, Object, Object> deleteTask =
+                                new AsyncTask<Long, Object, Object>()
+                                {
+                                    @Override
+                                    protected Object doInBackground(Long... params)
+                                    {
+                                        databaseConnector.deleteContact(params[0]);
+                                        return null;
+                                    } // end method doInBackground
+
+                                    @Override
+                                    protected void onPostExecute(Object result)
+                                    {
+                                        finish(); // return to the AddressBook Activity
+                                    } // end method onPostExecute
+                                }; // end new AsyncTask
+
+                        // execute the AsyncTask to delete contact at rowID
+                        deleteTask.execute(new Long[] { rowID });
+                    } // end method onClick
+                } // end anonymous inner class
+        ); // end call to method setPositiveButton
+
+        builder.setNegativeButton(R.string.button_cancel, null);
+        builder.show(); // display the Dialog
+    } // end method deleteContact
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == Activity.RESULT_OK) {
+            new SendContactTask().execute(new String[] {
+                    data.getExtras().getString(DeviceChooser.DEVICE_ADDRESS)
+            });
+        } else {
+            Toast.makeText(this, R.string.connection_error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private class SendContactTask extends AsyncTask<String,Object,Object> {
+
+        @Override
+        protected Object doInBackground(String... params) {
+            BluetoothDevice device = bluetoothAdapter.getRemoteDevice(params[0]);
+
+            BluetoothSocket bluetoothSocket = null;
+
+            try {
+                AddressBook.displayToastViaHandler(ViewContact.this, handler,
+                        R.string.sending_contact);
+
+                bluetoothSocket = device.createRfcommSocketToServiceRecord(
+                        AddressBook.MY_UUID
+                );
+
+                bluetoothSocket.connect();
+
+                OutputStream outputStream = bluetoothSocket.getOutputStream();
+
+                //create representative of contact
+                final JSONObject contact = new JSONObject();
+                contact.put("name", nameTextView.getText().toString());
+                contact.put("phone", phoneTextView.getText().toString());
+                contact.put("email", emailTextView.getText().toString());
+                contact.put("street", streetTextView.getText().toString());
+                contact.put("city", cityTextView.getText().toString());
+
+                outputStream.write(contact.toString().getBytes());
+                outputStream.flush();
+                AddressBook.displayToastViaHandler(ViewContact.this, handler,
+                R.string.contact_sent);
+            } catch (IOException e) {
+                e.printStackTrace();
+                AddressBook.displayToastViaHandler(ViewContact.this, handler,
+                        R.string.transfer_failed);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                AddressBook.displayToastViaHandler(ViewContact.this, handler,
+                        R.string.transfer_failed);
+            } finally {
+                try {
+                    bluetoothSocket.close();
+                } catch (IOException e) {
+                    Log.e(TAG, e.toString());
+                }
+                bluetoothSocket = null;
+            }
+
+            return null;
+        }
+    }
 }
